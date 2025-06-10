10b4b3006018a5c0d52c0c8f8694a7e4f19172c7
PujaChowdhury <PujaChowdhury@PSDLAP73>
2025-06-10 12:54:27 +0530

    Demo test suit

---
+++ Test Suites/Demo-git.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Demo-git</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>3c3544c9-5efc-4529-8879-765d6c91fc7d</testSuiteGuid>
+</TestSuiteEntity>
